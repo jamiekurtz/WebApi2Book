f3033a74420baf47b6ebb8afccfd9f703eb90db6
Brian Wortman <brian.wortman@outlook.com>
2014-07-21 09:30:12 -0400

    populated source tree

---
+++ src/WebApi2Book.Web.Api/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebApi2Book.Web.Api.WebApiApplication" Language="C#" %>
---
+++ src/WebApi2Book.Web.Api/Global.asax.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Routing;
+
+namespace WebApi2Book.Web.Api
+{
+    public class WebApiApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+        }
+    }
+}
