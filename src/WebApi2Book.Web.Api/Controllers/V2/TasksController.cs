31a5b4dd5056da22fc2ebf5ab9f4b89f9718787b
Brian Wortman <brian.wortman@outlook.com>
2014-07-21 11:49:04 -0400

    added tasks controllers

---
+++ src/WebApi2Book.Web.Api/Controllers/V2/TasksController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApi2Book.Web.Api.Controllers.V2
+{
+    public class TasksController : ApiController
+    {
+    }
+}
